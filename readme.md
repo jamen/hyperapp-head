2efbbf18db3e843c016d8de7138f1c3f26c0c249
Jamen Marz <me@jamenmarz.com>
2019-04-23 17:00:47 -0600

    Initial commit.

---
+++ readme.md
@@ -0,0 +1,27 @@
+# @finepoint/hyperapp-head
+
+Patch Hyperapp <head> nodes.
+
+## Install
+
+```
+npm i @finepoint/hyperapp-head
+```
+
+## Usage
+
+```js
+import { patchHead } from '@finepoint/hyperapp-head'
+
+patchHead(
+    <head>
+        <title>Hello world.</title>
+    </head>
+)
+```
+
+Here are some notes about the implementation:
+
+- It only patches `<title>`, `<meta>`, and `<link>` elements. There may be others (such as `<base>`) but they wont be patched.
+- It is not recursive because `<head>` is usually flat. It focuses on matching keys instead, such as `<meta name>`, `<meta property>`, `<link rel>`, and so on.
+- There is a `mergeHead(targetHead, head)` function for patching in tools or servers. For example, you have the site's <head> and the route page <head>, these would need be merged when prerendering.
---
+++ src/head.js
@@ -0,0 +1,42 @@
+// Used on route updates.
+export const patchHead = head => {
+    Object.assign(document.head, head.attributes)
+
+    for (const el of document.head.childNodes) {
+        for (const node of head.children) {
+            if (node.nodeName === 'title' && el.nodeName === 'TITLE') {
+                document.title = node.children[0]
+                Object.assign(el, node.attributes)
+            } else if (testMetaKeys(node, el)) {
+                Object.assign(el, node.attributes)
+            }
+        }
+    }
+}
+
+// Used for prerendering
+export const mergeHead = (targetHead, head) => {
+    Object.assign(targetHead.attributes, head.attributes)
+
+    for (const meta of head.children) {
+        for (const targetMeta of targetHead.children) {
+            if (targetMeta.nodeName === 'title' && meta.nodeName === 'title') {
+                targetMeta.children.push(...meta.children)
+                Object.assign(targetMeta.attributes, meta.attributes)
+            } if (testMetaKeys(targetMeta, meta)) {
+                Object.assign(targetMeta.attributes, meta.attributes)
+            }
+        }
+    }
+}
+
+// Keys to match <meta> and <link> by between updates.
+const metaKeys = [ 'name', 'property', 'itemprop', 'http-equiv', 'rel' ]
+
+const testMetaKeys = (node1, node2) => {
+    for (const key of metaKeys) {
+        if (node1[key] && node2[key] && node1[key] === node2[key]) {
+            return true
+        }
+    }
+}
